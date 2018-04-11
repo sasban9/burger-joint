1a8a8fb6c896fbf52f085c18064b7815487210aa
Saswata Banerjee <saswata@talasecure.com>
2018-04-11 22:47:10 +0530

    Dynamic ingredients init.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	const transIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])] // [x,y]
+			.map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />
+			})
+		});
+
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
