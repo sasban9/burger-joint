a5b8a601573ae5d1cbad3633f8503347a4bddef1
Saswata Banerjee <saswata@talasecure.com>
2018-04-17 18:26:38 +0530

    Adding global error handling.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -100,7 +101,7 @@ class BurgerBuilder extends Component {
       deliveryMethod: 'fastest'
     };
     axios
-      .post('/orders.json', order)
+      .post('/orders.json', order) // remove .json to fail the POST call
       .then(response => {
         this.setState({ loading: false, purchasing: false });
       })
@@ -149,4 +150,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
